98b1b33089bce7e6f550854afa4a388b535b804b
deviprsd <deviprsd21@gmail.com>
2019-06-14 03:08:40 -0500

    bare minimum init

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Devi Prasad',
+    author_email='deviprsd21@gmail.com',
+    description='',
+    name='SubStar',
+    license='GPLv3+',
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    platforms='any',
+    url='https://github.com/titans-inc/substar-python',
+    version='0.0.1-alpha',
+    classifiers=[
+        'Programming Language :: Python'
+    ]
+)
